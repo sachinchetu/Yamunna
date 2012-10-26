1c91c1c84ff6999042cb948074345ebda4bcffdd
sachinchetu <sachin_java2@yahoo.co.in>
2012-10-27 01:30:20 +0530

    commiting chnages

--- YamunaCRM/src/com/yamunacrm/action/ForgetPasswordAction.java
+++ YamunaCRM/src/com/yamunacrm/action/ForgetPasswordAction.java
@@ -5,46 +5,58 @@
 package com.yamunacrm.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-import com.yamunacrm.dao.UserDAO;
-import com.yamunacrm.dao.daoimpl.UserDAOImpl;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+import com.opensymphony.xwork2.validator.annotations.Validations;
+import com.opensymphony.xwork2.validator.annotations.ValidatorType;
+import com.yamunacrm.service.LoginService;
 import org.apache.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.InterceptorRef;
+import org.apache.struts2.convention.annotation.InterceptorRefs;
+import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 
 /**
  *
  * @author udays
  */
+@ParentPackage("struts-alternate")
+@InterceptorRefs({
+    @InterceptorRef("jsonValidationWorkflowStack"),
+    @InterceptorRef("closeHibernateInterceptor")
+})
+        @Validations(requiredStrings = {
+    @RequiredStringValidator(fieldName = "userName", type = ValidatorType.FIELD, message = " is required")
+    }
+)
+
 public class ForgetPasswordAction extends ActionSupport {
 
   private static final long serialVersionUID = 7968544374444173511L;
    private static final Logger log=Logger.getLogger(ForgetPasswordAction.class);
   private String            userName;
-  private String              message;
   @Action(value = "/forgetPassword", results = {
-    @Result(location = "/jsp/main/message.jsp", name = "success"),@Result(location="/jsp/main/message.jsp",name="error")
+    @Result(location = "/jsp/main/message.jsp", name = "success"),@Result(location="/jsp/main/message.jsp",name="error"),@Result(location="/index.jsp",name="input")
   })
     @Override
   public String execute() throws Exception
   {
       log.info("User "+userName);
-    UserDAO userDao=new UserDAOImpl();
-    if(!userDao.checkAvailbility(userName))
+    LoginService loginService=new LoginService();
+    if(userName==null)
     {
-       setMessage("User is not available.");
+    addFieldError(userName, "User Name required.");
+    return ERROR;
+    }
+    if(!loginService.checkAvaility(userName))
+    {
+       addActionError("User is not available.");
        return ERROR;
     }else{
-        setMessage("Please contact your administrator to get the password.");
+        addActionMessage("Please contact your administrator to get the password.");
         return SUCCESS;
     }
   }
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
     public String getUserName() {
         return userName;
     }
